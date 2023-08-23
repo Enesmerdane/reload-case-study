e4f06ffdd3712c54276803d766fccd5e24e18f88
enesmerdane <ensmerdane@gmail.com>
2023-08-23 21:51:06 +0300

    message box created
    message box created

---
+++ src/components/messageBox/index.tsx
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+import ChatGPTLogo from "../../assets/ChatGPT_logo.png";
+
+interface MessageInterface {
+    sentByUser: boolean;
+    messageText: string;
+}
+
+function MessageBox(props: MessageInterface) {
+    let senderTitle = props.sentByUser ? "You" : "Chat GPT";
+
+    return (
+        <div
+            className={`chatpage__message_box ${
+                !props.sentByUser ? "response" : null
+            }`}
+        >
+            <img
+                src={!props.sentByUser ? ChatGPTLogo : null}
+                className="chatpage_message_box_img"
+            ></img>
+            <div className="chatpage_message_box_text">
+                <code>{senderTitle}</code>
+                <div className="chatpage_message_box_text_content">
+                    {props.messageText}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MessageBox;
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -3,10 +3,20 @@ import "./styles.css";
 import Arrow_little from "../../utils/arrow_little";
 import Logo from "../../utils/logo";
 import SendButton from "../../utils/message_send_button";
+import MessageBox from "../../components/messageBox";
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+interface MessageInterface {
+    sentByUser: boolean;
+    messageText: string;
+}
 
 function Chat() {
+    const [textInput, setInput] = useState("");
+    const [messages, setMessages] = useState<MessageInterface[]>([]);
+
     return (
         <div className="ChatPageBody">
             <div className="chatpage__section1">
@@ -25,16 +35,35 @@ function Chat() {
             <div className="chatpage__section2">
                 <Logo classname="chatpage__section2_logo" />
                 <div className="chatpage_chat_screen">
-                    <div className="chatpage__message_container"></div>
                     <div className="chatpage__input_field">
                         {/* <input /> */}
-                        <textarea placeholder="Enter your message" />
+                        <textarea
+                            placeholder="Enter your message"
+                            value={textInput}
+                            onChange={(e) => {
+                                setInput(e.target.value);
+                            }}
+                        />
                         <SendButton
                             classname="chatpage__send_button"
                             onclick={() => {
-                                window.alert("sent yey");
+                                window.alert(textInput);
                             }}
                         />
+                    </div>
+                    <div className="chatpage__message_container">
+                        {messages.map((message, id) => (
+                            <MessageBox
+                                sentByUser={message.sentByUser}
+                                messageText={message.messageText}
+                            />
+                        ))}
+                        <MessageBox sentByUser={true} messageText="yes" />
+                        <MessageBox sentByUser={true} messageText="yes" />
+                        <MessageBox sentByUser={true} messageText="yes" />
+                        <MessageBox sentByUser={true} messageText="yes" />
+                        <MessageBox sentByUser={true} messageText="yes" />
+                        <MessageBox sentByUser={true} messageText="yes" />
                     </div>
                 </div>
             </div>
